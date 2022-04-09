417bbd8377aeb9b2fd74c4ea414949f6b9978241
Quinn <90532540+QuinnZhong@users.noreply.github.com>
2022-04-09 01:25:13 -0400

    add js and some details

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-main.js
+/*-- bio card js --*/
+const cards = getAll('[data-js=card]')
+cards.forEach(card => {
+  const content = card.querySelector('p')
+  card.addEventListener('click', () => {
+    content.classList.toggle('hidden')
+  })
+  
+})
+function getAll(selector) {
+  return document.querySelectorAll(selector)
+}
